d72fd8307144515789121491b7f707615712a347
Горин Александр <pu1s@outlook.com>
2020-01-24 18:52:19 +0400

    new UI control

---
+++ MedCalcLib/UI/WatermarkTextBox.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MedCalc
+{
+    /// <summary>
+    /// Логика взаимодействия для WatermarkTextBlock.xaml
+    /// </summary>
+    public partial class WatermarkTextBox : UserControl
+    {
+        public WatermarkTextBox()
+        {
+            InitializeComponent();
+        }
+
+
+        public string WatermarkText
+        {
+            get { return (string)GetValue(WatermarkTextProperty); }
+            set { SetValue(WatermarkTextProperty, value); }
+        }
+
+        
+        public static readonly DependencyProperty WatermarkTextProperty =
+            DependencyProperty.Register("WatermarkText", typeof(string), typeof(WatermarkTextBox), new PropertyMetadata("Watermark"));
+
+
+  
+
+    }
+}
