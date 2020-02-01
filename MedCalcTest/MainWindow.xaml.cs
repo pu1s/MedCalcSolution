6ca22912c728bfa2f1d19771c640bc55f647092d
Горин Александр <pu1s@outlook.com>
2020-02-01 19:11:13 +0400

    ++++++++

--- MedCalcTest/MainWindow.xaml.cs
+++ MedCalcTest/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using MedCalc.UI;
+//using MedCalc.UI;
 
 namespace MedCalcTest
 {
@@ -29,9 +29,6 @@ namespace MedCalcTest
             
         }
 
-        private void WMT_GotFocus(object sender, RoutedEventArgs e)
-        {
-
-        }
+        
     }
 }
